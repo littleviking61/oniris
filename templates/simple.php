fe009f3ce752f8e435181f582139f2d401aca076
Baptiste Régné <baptiste.regne@yahoo.fr>
2015-01-15 00:39:04 +0100

    remove INK + style paragraphe

--- templates/simple.php
+++ templates/simple.php
@@ -1,25 +1,40 @@
 <?php 
-	$width = get_sub_field('largeur_du_bloc');
 	$title = get_sub_field('titre');
-	$image = get_sub_field('image');
+
+	$width = get_sub_field('largeur_du_bloc');
 	$specificClass = get_sub_field('specifique_bloc_class');
+
+	$hasImage = get_sub_field('has_image');
+	$image = get_sub_field('image');
 	$imagePosition = get_sub_field('position');
+	
+	$hasButton = get_sub_field('has_bouton');
+	$linkButton = get_sub_field('lien_du_bouton');
+	$textButton = get_sub_field('texte_du_bouton');
+	// $buttonPosition = get_sub_field('emplacement_du_bouton');
 ?>
 
 <section class="container simple <?= $width > 0 ? 'flex-' . $width : 'full' ?> <?= $specificClass ?>">
 
-	<?php if (!empty($image)): ?>
+	<?php if ($hasImage && !empty($image)): ?>
 		<div class="thumbnail <?= $imagePosition ?>">
 			<img src="<?= $image ?>">
 		</div>
 	<?php endif ?>
 
 	<div class="content">
+		
 		<?php if (!empty($title)): ?>
 			<h2 class="title"><?= $title ?></h2>
 		<?php endif ?>
 
 		<?php the_sub_field('texte'); ?>
+
+		<?php if ($hasButton && !empty($linkButton) && !empty($textButton) ): ?>
+			<a class="button" href="<?= $linkButton ?>"><?= $textButton ?></a>
+		<?php endif ?>
+
 	</div>
+
 
 </section>
