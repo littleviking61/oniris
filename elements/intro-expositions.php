7ff192844637d8c0e54cdad4b32dea9ca35f4560
Baptiste Régné <baptiste.regne@yahoo.fr>
2015-03-13 14:59:58 +0100

    intro exposition multi artistes

--- elements/intro-expositions.php
+++ elements/intro-expositions.php
@@ -1,12 +1,27 @@
-<h2><?= get_field('nom') ?></h2>
+<?php 
+	$artistes = get_field('relation_artistes');
 
-<section class="container flex-2 intro">
-	<?php if( has_post_thumbnail() ): ?>
-		<div class="thumbnail">
-			<?php the_post_thumbnail('small') ?>
-		</div>
+?>
+
+<div class="row"><h2 class="flex-3"><?= get_field('nom') ?></h2></div>
+<section class="flex-3 intro expo">
+	<hr>
+	<?php if (get_field('intitule')): ?>
+		<h4 class="intitule"><?php do_shortcode(the_field('intitule')) ?></h4>
+		<hr>
 	<?php endif ?>
-	<div class="content">
-		<?php do_shortcode(the_field('courte_biographie')) ?>
-	</p>
+
+	<?php if (get_field('sous_titre')): ?>
+		<h4 class="uncolor"><?= do_shortcode(get_field('sous_titre')) ?></h4>
+	<?php endif ?>
+
+	<ul class="artistes">
+		<?php foreach ($artistes as $artiste): ?>
+
+			<?php if (get_field('nom', $artiste)): ?>
+				<li><a href="<?= get_permalink($artiste) ?>"><?= get_field('prenom', $artiste) ?> <?= get_field('nom', $artiste) ?></a></li>
+			<?php endif ?>
+			
+		<?php endforeach ?>
+	</ul>
 </section>
