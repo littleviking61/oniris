6543f626d7242a88ade9dcac7728ba28dfee46de
Baptiste Régné <baptiste.regne@gmail.com>
2016-09-02 11:55:38 +0200

    header infos plus style

--- header.php
+++ header.php
@@ -17,24 +17,40 @@
 
 	<header class="main" role="banner">
 		<!-- Starting the nav -->
-		<div class="banner">
-			<h1><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>">
-				<?php include(locate_template('icon.php')) ?>
-				<span class="hide"><?php bloginfo('name'); ?></span></a>
-			</h1>
-			<!-- <div class="switch-lang">
-				<a href="#" class="fr active">FR</a> / <a href="#" class="en">EN</a>
-			</div> -->
+		<div class="header-info">
+			<div class="header-content">
+				<?php wp_nav_menu( array( 
+					'container' => false,
+					'menu_class' => 'header-social',
+					'theme_location' => 'social' )
+				); ?>
+
+				<div class="infos">
+					<?php the_field('contenu_du_header', 'option'); ?>
+				</div>
+			</div>
 		</div>
+        
+		<div class="header-content header-banner">
+			<div class="banner">
+				<h1><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>">
+					<?php include(locate_template('icon.php')) ?>
+					<span class="hide"><?php bloginfo('name'); ?></span></a>
+				</h1>
+				<!-- <div class="switch-lang">
+					<a href="#" class="fr active">FR</a> / <a href="#" class="en">EN</a>
+				</div> -->
+			</div>
 
-		<?php wp_nav_menu( array( 
-			'container' => 'nav',
-			'container_class' => 'main hide-for-small',
-			'theme_location' => 'primary' )); 
-		?>
+			<?php wp_nav_menu( array( 
+				'container' => 'nav',
+				'container_class' => 'main hide-for-small',
+				'theme_location' => 'primary' )); 
+			?>
 
-		<span class="show-for-medium show-menu" data-show="#aside"><?= __('Navigation rapide') ?></span>
+			<span class="show-for-medium show-menu" data-show="#aside"><?= __('Navigation rapide') ?></span>
 
+		</div>
 		<!-- End of Top-Bar -->
 	</header>
 
